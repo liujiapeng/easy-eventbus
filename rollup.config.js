7fa2c9be4091ba85848f2171fb5feb959ad5b521
pipedri:mə <47778412+liujiapeng@users.noreply.github.com>
2021-04-01 12:22:48 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -9,13 +9,18 @@ export default [
     input: 'src/index.ts',
     output: {
       name: 'howLongUntilLunch',
-			file: pkg.main,
-			format: 'umd'
+      file: pkg.main,
+      format: 'umd',  // for cmd & amd like webpack project or node project
     },
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript()
-    ]
+    plugins: [resolve(), commonjs(), typescript()],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'howLongUntilLunch',
+      file: pkg.module, // for es module like <script type="module"> import EventBus from './lib/index.esm.js'</script>
+      format: 'esm',
+    },
+    plugins: [resolve(), commonjs(), typescript()],
   },
 ]
